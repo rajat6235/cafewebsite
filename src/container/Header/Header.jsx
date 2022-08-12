04c6121f59637921b021fd81f5c30cb1bbbeba29
rajat6235 <guptarajat234@gmail.com>
2022-08-12 11:43:46 +0530

    old images header removed

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,11 +9,6 @@ import './Header.css';
 const Header = () => {
 
   const slides = [
-    // "https://picsum.photos/id/1032/900/400",
-    // "https://picsum.photos/id/1033/900/400",
-    // "https://picsum.photos/id/1037/900/400",
-    // "https://picsum.photos/id/1035/900/400",
-    // "https://picsum.photos/id/1036/900/400",
     images.headerimg1,
     images.headerimg2,
     images.headerimg3,
