03e714ee4eb287a8642b3275f2b7332afd32bf52
rajat6235 <guptarajat234@gmail.com>
2022-08-24 12:30:27 +0530

    takeaway menu whatsapp logo added

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -31,9 +31,30 @@ const SpecialMenu = () => (
 
       </div>
 
-      <div className="app__specialMenu-menu_img">
-        <img src={images.menu} alt="menu__img" />
+      <div className="app__specialMenu-menu_wine  flex__center takeAway">
+        <p className="app__specialMenu-menu_heading">TAKEAWAY MENU</p>
+        <div className="app__specialMenu_menu_items">
+          {/* {data.wines.map((wine, index) => (
+            <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
+          ))} */}
+        To order your fresh, healthy takeaway food for collection from the café,
+           <br/>
+        simply call
+         <a href="tel:9888861516" className='PhoneNum'>9888861516</a><a href="tel:9888861517"className='PhoneNum'>9888861517</a>
+        <br/>
+        or Whatsapp us on  <a href="tel:9888861517"className='PhoneNum'> <i class="fa fa-whatsapp fa-lg"></i>9888861517 </a>
+        </div>
+        <div className='orderButtonsContainer'>
+      <button type="button" className="takeawaymenu__button">View TakeAway Menu</button>
+        <a id="button" className='orderButton' href="https://gymjunkiecafe.es/"></a>
+        {/* <a id="button" className='orderButton' href="https://gymjunkiecafe.es/"> <p className='ordertext'>ORDER ON</p> 	&nbsp;<img className='swiggyIcon' src={images.swiggy}/></a> */}
+       {/* <div className='robContainer'> <a id="button" className='orderButton' href="https://gymjunkiecafe.es/"><p className='ordertext'>ORDER ON</p>&nbsp;<img className='robIcon' src={images.logo1}/> </a><p className='discount'>(&nbsp;23% off on <br/> online menu)</p></div> */}
+        </div>
+
       </div>
+      {/* <div className="app__specialMenu-menu_img">
+        <img src={images.menu} alt="menu__img" />
+      </div> */}
 
       <div className="app__specialMenu-menu_cocktails  flex__center">
         <p className="app__specialMenu-menu_heading">Cocktails</p>
