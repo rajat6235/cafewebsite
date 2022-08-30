bb03bfa2364dc163e5d969df5214ac959ef7bc70
rajat6235 <guptarajat234@gmail.com>
2022-08-30 07:22:33 +0530

    send button loading fix

--- robusterscafe1/src/components/Footer/Newsletter.jsx
+++ robusterscafe1/src/components/Footer/Newsletter.jsx
@@ -1,13 +1,11 @@
-
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
-import { images } from '../../constants';
-import SubHeading from '../SubHeading/SubHeading';
-import './Newsletter.css';
+import { images } from "../../constants";
+import SubHeading from "../SubHeading/SubHeading";
+import "./Newsletter.css";
 
 const Newsletter = () => {
-  
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
@@ -20,7 +18,6 @@ const Newsletter = () => {
   const handleMessage = (e) => {
     setMessage(e.target.value);
   };
-
 
   const submitForm = async (e) => {
     e.preventDefault();
@@ -36,14 +33,10 @@ const Newsletter = () => {
         setBanner(res.data.msg);
         toast.error(res.data.msg);
         setBool(false);
-      } 
-      else if  (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
-    
-      {  
+      } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
         toast.error(res.data.msg);
         setBool(false);
-       }
-      else if (res.status === 200) {
+      } else if (res.status === 200) {
         setBanner(res.data.msg);
         toast.success(res.data.msg);
         setBool(false);
@@ -56,28 +49,43 @@ const Newsletter = () => {
     }
   };
 
-  return(
-  <div className="app__newsletter">
-    <div className="app__newsletter-heading">
-      <SubHeading title="Contact Us" />
-      <h1 className="headtext__cormorant newsletterText">Leave Your Precious Feedback</h1>
-      {/* <p className="p__opensans">And never miss latest Updates!</p> */}
+  return (
+    <div className="app__newsletter">
+      <div className="app__newsletter-heading">
+        <SubHeading title="Contact Us" />
+        <h1 className="headtext__cormorant newsletterText">
+          Leave Your Precious Feedback
+        </h1>
+        {/* <p className="p__opensans">And never miss latest Updates!</p> */}
+      </div>
+      <input
+        type="email"
+        placeholder="Enter your email address"
+        className="email"
+        onChange={handleEmail}
+        value={email}
+      />
+      <div className="app__newsletter-input flex__center">
+        <textarea
+          type="text"
+          placeholder="Type your message here"
+          onChange={handleMessage}
+          value={message}
+        />
+        <button type="button" className="custom__button sendButton" onClick={submitForm}>
+          {bool ? (
+            <b className="load">
+              <img src={images.load2} alt="not responding" />
+            </b>
+          ) : (
+            <p>
+              Send
+              <i className="fa fa-paper-plane plane" />
+            </p>
+          )}
+        </button>
+      </div>
     </div>
-    <input type="email" placeholder="Enter your email address" className='email' onChange={handleEmail} value={email} />
-    <div className="app__newsletter-input flex__center">
-      <textarea type="text" placeholder="Type your message here" onChange={handleMessage} value={message}/>
-      <button type="button" className="custom__button" onClick={submitForm}>Send
-      <i className="fa fa-paper-plane" />
-                {bool ? (
-                  <b className="load">
-                    <img src={images.load2} alt="not responding" /> 
-                  </b>
-                ) : (
-                  ""
-                )}
-      </button>
-    </div>
-  </div>
-);
-}
+  );
+};
 export default Newsletter;
